a50a3bfa96d2ea5d791cb742ca6216bdcfb5edb1
andreimusan <60709013+andreimusan@users.noreply.github.com>
2020-03-18 10:41:03 +0200

    Add files via upload

---
+++ week 6/JS DOM Manipulation/app.js
@@ -0,0 +1,74 @@
+const btn = document.querySelector('#btn-submit');
+const fname = document.querySelector('#fname');
+const lanme = document.querySelector('#lname');
+const msg = document.querySelector('#message');
+const closeBtn = document.querySelector('.btn-close');
+
+btn.addEventListener('click', function(){
+    event.preventDefault();
+    let first = validateTextInput(fname);
+    let second = validateTextInput(lname);
+    let message = validateMessageInput(msg);
+    let gender = validateRadioButton();
+    
+    if(first && second && message && gender) {
+        console.log("First Name: " + fname.value);
+        console.log("Last Name: " + lname.value);
+        console.log("Gender: " + gender);
+        console.log("Message: " + msg.value);
+        submitSuccessful();
+    }
+});
+
+function submitSuccessful() {
+    let element = document.querySelector('#green-label');
+    let paragraph = document.querySelector('#green-label>p');
+    let text = "Thank you for contacting us, ";
+    paragraph.innerHTML = text + fname.value;
+    element.classList.remove('form-successful-hidden');
+    element.classList.add('form-successful-active');
+    document.querySelector('.form').reset();
+}
+
+function validateTextInput(name) {
+    let letters = /^[A-Za-z]+$/;
+    
+    if (name.value == "" || name.length <= 2 || !name.value.match(letters)) {
+        name.classList.add('error-red-border');
+    } else {
+        name.classList.remove('error-red-border');
+        return true;
+    }
+}
+
+function validateRadioButton(){
+    var radios = document.getElementsByName('gender');
+
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            document.querySelector('.gender').style.color = "black";
+            return radios[i].value;
+        }
+    }
+
+    document.querySelector('.gender').style.color = "red";
+}
+
+function validateMessageInput(msg) {
+    if (msg.value == "") {
+        msg.classList.add('error-red-border');
+    } else {
+        msg.classList.remove('error-red-border');
+        return true;
+    }
+}
+
+closeBtn.addEventListener('click', function(){
+    closeSubmitSuccessful();
+});
+
+function closeSubmitSuccessful() {
+    let element = document.querySelector('#green-label');
+    element.classList.remove('form-successful-active');
+    element.classList.add('form-successful-hidden');
+}
